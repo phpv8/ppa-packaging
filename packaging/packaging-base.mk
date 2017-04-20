5d4e2e4aadd4339c6972084cd81d05d0da1218eb
Bogdan Padalko <pinepain@gmail.com>
2017-04-20 22:05:19 +0300

    v8@6.0 6.0.45

--- packaging/packaging-base.mk
+++ packaging/packaging-base.mk
@@ -33,56 +33,57 @@ install: build
 
 _build: distro
 	\
-if test -z "$$DEBEMAIL" -o -z "$$DEBFULLNAME"; then \
-  echo "DEBFULLNAME and DEBEMAIL environmental variable should be set" ; \
-  echo "For example:" ; \
-  echo "export DEBEMAIL=\"my@emailaddress.com\"" ; \
-  echo "export DEBFULLNAME=\"Full Name\"" ;\
-  exit 1; \
-fi
-	\
-cd "work/${NAME}_${VERSION}" ; \
-for distro in ${DISTROS}; do \
-  NEW_VER="${VERSION}-ppa${PPA_VERSION}~$$distro"; \
-  rm -Rf debian ; cp -r ../../debian . ; \
-  sed -i -e "s/DISTRO/$$distro/g" debian/changelog ; \
-  for file in debian/*.$$distro; do \
-    if [ -f $$file ]; then \
-      rename -f "s/\.$$distro$$//" $$file ; \
-    fi ; \
-  done ; \
-  CUR_NAME=`dpkg-parsechangelog | grep '^Source: ' | awk '{print $$2}'`; \
-  CUR_VER=`dpkg-parsechangelog | grep '^Version: ' | awk '{print $$2}'`; \
-  if dpkg --compare-versions $$NEW_VER gt $$CUR_VER; then \
-    echo "New version. Will update changelog and build source package" ; \
-    dch -v $$NEW_VER --package="${NAME}" -D $$distro --force-distribution \
-        "New version based on ${GIT_VERSION} (${GIT_URL})" ; \
-    DEB_MAINTAINER_MODE=1 debuild clean ; \
-  else \
-    if dpkg --compare-versions $$NEW_VER ne $$CUR_VER; then \
-      echo "ERROR: Cannot rebuild source package, because new version is earlier \
-than the one specified in changelog ($$NEW_VER < $$CUR_VER)" ; \
-      exit 1; \
-    fi ; \
-    echo "Same version, just rebuild source package" ; \
-  fi ; \
-  ${DEBUILD} ; \
-done
+	if test -z "$$DEBEMAIL" -o -z "$$DEBFULLNAME"; then \
+	  echo "DEBFULLNAME and DEBEMAIL environmental variable should be set" ; \
+	  echo "For example:" ; \
+	  echo "export DEBEMAIL=\"my@emailaddress.com\"" ; \
+	  echo "export DEBFULLNAME=\"Full Name\"" ;\
+	  exit 1; \
+	fi
+		\
+	cd "work/${NAME}_${VERSION}" ; \
+	for distro in ${DISTROS}; do \
+	  NEW_VER="${VERSION}-ppa${PPA_VERSION}~$$distro"; \
+	  rm -Rf debian ; cp -r ../../debian . ; \
+	  sed -i -e "s/DISTRO/$$distro/g" debian/changelog ; \
+	  for file in debian/*.$$distro; do \
+	    if [ -f $$file ]; then \
+	      rename -f "s/\.$$distro$$//" $$file ; \
+	    fi ; \
+	  done ; \
+	  CUR_NAME=`dpkg-parsechangelog | grep '^Source: ' | awk '{print $$2}'`; \
+	  CUR_VER=`dpkg-parsechangelog | grep '^Version: ' | awk '{print $$2}'`; \
+	  if dpkg --compare-versions $$NEW_VER gt $$CUR_VER; then \
+	    echo "New version. Will update changelog and build source package" ; \
+	    dch -v $$NEW_VER --package="${NAME}" -D $$distro --force-distribution \
+	        "New version based on ${GIT_VERSION} (${GIT_URL})" ; \
+	    DEB_MAINTAINER_MODE=1 debuild clean ; \
+	  else \
+	    if dpkg --compare-versions $$NEW_VER ne $$CUR_VER; then \
+	      echo "ERROR: Cannot rebuild source package, because new version is earlier \
+	            than the one specified in changelog ($$NEW_VER < $$CUR_VER)" ; \
+	      exit 1; \
+	    fi ; \
+	    echo "Same version, just rebuild source package" ; \
+	  fi ; \
+	  ${DEBUILD} ; \
+	done
 
 dput: source-build
 	\
-cd "work" ; \
-for distro in ${DISTROS}; do \
-  dput -f "${PPA}" "${NAME}_${VERSION}-ppa${PPA_VERSION}~$$distro""_source.changes" ; \
-done ; \
-\
-cd .. ; \
-NEW_VER="${VERSION}-ppa${PPA_VERSION}~DISTRO"; \
-CUR_VER=`dpkg-parsechangelog | grep '^Version: ' | awk '{print $$2}'`; \
-if dpkg --compare-versions $$NEW_VER gt $$CUR_VER; then \
-  dch -v $$NEW_VER --package="${NAME}" -D DISTRO --force-distribution \
-      "New version based on ${GIT_VERSION} (${GIT_URL})" ; \
-fi
+	cd "work" ; \
+	for distro in ${DISTROS}; do \
+	  dput -f "${PPA}" "${NAME}_${VERSION}-ppa${PPA_VERSION}~$$distro""_source.changes" ; \
+	done ; \
+	\
+	cd .. ; \
+	NEW_VER="${VERSION}-ppa${PPA_VERSION}~DISTRO"; \
+	CUR_VER=`dpkg-parsechangelog | grep '^Version: ' | awk '{print $$2}'`; \
+	if dpkg --compare-versions $$NEW_VER gt $$CUR_VER; then \
+	  dch -v $$NEW_VER --package="${NAME}" -D DISTRO --force-distribution \
+	      "New version based on ${GIT_VERSION} (${GIT_URL})" ; \
+	  DEB_MAINTAINER_MODE=1 debuild clean ; \
+	fi
 
 clean:
 	@rm -Rf work
