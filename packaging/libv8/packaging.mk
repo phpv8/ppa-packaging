f76170bf1d774963977f3bbc2b6fdeb4cbbdf993
Bogdan Padalko <pinepain@gmail.com>
2017-09-09 23:48:04 +0300

    Make libv8 building less verbose to match travis log limit [skip ci]

--- packaging/libv8/packaging.mk
+++ packaging/libv8/packaging.mk
@@ -17,7 +17,7 @@ work/${NAME}_${VERSION}:
 	cp ./../../Makefile.target Makefile ; \
 	git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git ; \
 	export PATH=`pwd`/depot_tools:"${PATH}" ; \
-	gclient sync -vvv -j ${NPROCS} -r ${VERSION} ; \
+	gclient sync -j ${NPROCS} -r ${VERSION} ; \
 	cd .. ; \
 	tar --exclude='./debian' --exclude .git --exclude '*.pyc' -cf - ${NAME}_${VERSION} | gzip -n9c > ${NAME}_${VERSION}.orig.tar.gz ; \
 
